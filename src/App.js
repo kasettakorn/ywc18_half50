19ddc3ec1cc6e7bbe746f93222177e01d39a3a50
kasettakorn <kasettakorn_techno@hotmail.com>
2020-11-17 21:54:36 +0700

    Add responsive

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
-import { Tag, Col, Input, Layout, Radio, Row, Select, Breadcrumb, Rate } from "antd";
-import { PushpinFilled } from '@ant-design/icons';
+import {
+  Tag,
+  Col,
+  Input,
+  Layout,
+  Radio,
+  Row,
+  Select,
+  Breadcrumb,
+  Rate,
+} from "antd";
+import { PushpinFilled } from "@ant-design/icons";
 import "antd/dist/antd.css";
-import './styles/style.css';
-import './index.css';
+import "./styles/style.css";
+import "./index.css";
 import data from "./data/store.json";
 import logo from "./assets/halfhalf-logo.png";
 import miniLogo from "./assets/halfhalf-logo-mini.png";
 import Search from "antd/lib/input/Search";
-
 
 const { Header, Sider, Content } = Layout;
 const { Option } = Select;
@@ -19,8 +28,8 @@ export default class App extends Component {
       shopType: -1,
       province: null,
       priceRange: null,
-      subType: null
-    }
+      subType: null,
+    };
     this.handleShopTypeChange = this.handleShopTypeChange.bind(this);
     this.handleProvinceChange = this.handleProvinceChange.bind(this);
     this.handlePriceRangeChange = this.handlePriceRangeChange.bind(this);
@@ -29,28 +38,27 @@ export default class App extends Component {
 
   handleShopTypeChange = (e) => {
     this.setState({
-      shopType: e.target.value
-    })
-    
-  }
+      shopType: e.target.value,
+    });
+  };
 
   handleProvinceChange = (value) => {
     this.setState({
-      province: value
-    })
-  }
+      province: value,
+    });
+  };
 
   handlePriceRangeChange = (value) => {
     this.setState({
-      priceRange: value
-    })
-  }
+      priceRange: value,
+    });
+  };
 
   handleSubTypeChange = (e) => {
     this.setState({
-      subType: e.target.value
-    })
-  }
+      subType: e.target.value,
+    });
+  };
 
   render() {
     const radioStyle = {
@@ -59,50 +67,72 @@ export default class App extends Component {
       lineHeight: "30px",
     };
     return (
-      <Layout style={{backgroundColor:"transparent", padding:"20px"}}>
+      <Layout style={{ backgroundColor: "transparent" }}>
         <Header style={{ backgroundColor: "transparent" }}>
-            <Row >
-                <Col span={4}>
-                    <img className="full-logo" src={logo} alt="logo"  />
-                    <img className="mini-logo" src={miniLogo} alt="minilogo"  />
-                    
-                </Col>
-                <Col span={16}>
-                    <Input.Group compact>
-                        <Select defaultValue="พื้นที่ใกล้ฉัน" style={{ width: "25%"}} onChange={this.handleProvinceChange}>
-                            <Option key="พื้นที่ใกล้ฉัน" value="พื้นที่ใกล้ฉัน"><PushpinFilled /> พื้นที่ใกล้ฉัน</Option>
-                            <Option key="สถานที่ทั้งหมด" value="สถานที่ทั้งหมด"><PushpinFilled /> สถานที่ทั้งหมด</Option>
-                            {data.provinces.map(province => {
-                                return <Option key={province} value={province}>{province}</Option>;
-                            })}
-                        </Select>
-                         <Search  style={{ width: "60%" }} placeholder="ค้นหาชื่อ ร้านอาหาร และเครื่องดื่ม"  />
-                    </Input.Group>
-                                    
-                </Col>
-            
-                
-                
-            </Row>
-            <Row>
-                <Col span={24}>
-                    <Breadcrumb style={{ margin: '16px 0', backgroundColor:"rgb(41,59,120)", color:"white", padding:"10px" }}>
-                        <Breadcrumb.Item>หน้าแรก</Breadcrumb.Item>
-                        <Breadcrumb.Item>ค้นหา</Breadcrumb.Item>
-                        
-                    </Breadcrumb> 
-                    
-                </Col>
-              
-            </Row>
+          <Row gutter={16}>
+            <Col md={5}>
+              <img className="full-logo" src={logo} alt="logo" />
+              <img className="mini-logo" src={miniLogo} alt="minilogo" />
+            </Col>
+            <Col md={19} style={{marginTop:"20px"}}>
+              <Input.Group compact>
+                <Select
+                  defaultValue="พื้นที่ใกล้ฉัน"
+                  style={{ width: "15%" }}
+                  onChange={this.handleProvinceChange}
+                >
+                  <Option key="พื้นที่ใกล้ฉัน" value="พื้นที่ใกล้ฉัน">
+                    <PushpinFilled /> พื้นที่ใกล้ฉัน
+                  </Option>
+                  <Option key="สถานที่ทั้งหมด" value="สถานที่ทั้งหมด">
+                    <PushpinFilled /> สถานที่ทั้งหมด
+                  </Option>
+                  {data.provinces.map((province) => {
+                    return (
+                      <Option key={province} value={province}>
+                        {province}
+                      </Option>
+                    );
+                  })}
+                </Select>
+                <Search
+                  style={{ width: "85%" }}
+                  placeholder="ค้นหาชื่อ ร้านอาหาร และเครื่องดื่ม"
+                />
+              </Input.Group>
+            </Col>
+          </Row>
 
-           
-          
+          <Breadcrumb
+            style={{
+              margin: "16px 0",
+              backgroundColor: "rgb(41,59,120)",
+              color: "white",
+              padding: "10px",
+            }}
+          >
+            <Breadcrumb.Item>หน้าแรก</Breadcrumb.Item>
+            <Breadcrumb.Item>ค้นหา</Breadcrumb.Item>
+          </Breadcrumb>
         </Header>
 
-        <Layout style={{backgroundColor:"transparent", marginTop:"120px"}}>
-            
-          <Sider id="sider" width={300}  style={{height:"max-content", backgroundColor: "white", padding: "10px", border:"0.5px solid black"}}>
+        <Layout
+          style={{
+            backgroundColor: "transparent",
+            marginTop: "120px",
+            padding: "15px",
+          }}
+        >
+          <Sider
+            id="sider"
+            width={300}
+            style={{
+              height: "max-content",
+              backgroundColor: "white",
+              padding: "10px",
+              border: "0.5px solid black",
+            }}
+          >
             <div>ประเภทร้านค้า</div>
             <Radio.Group onChange={this.handleShopTypeChange}>
               <Radio key={-1} style={radioStyle} value={-1}>
@@ -116,50 +146,123 @@ export default class App extends Component {
                 );
               })}
             </Radio.Group>
-            <Select defaultValue="พื้นที่ใกล้ฉัน" style={{ width: "100%" }} onChange={this.handleProvinceChange}>
-              <Option key="พื้นที่ใกล้ฉัน" value="พื้นที่ใกล้ฉัน"><PushpinFilled /> พื้นที่ใกล้ฉัน</Option>
-              <Option key="สถานที่ทั้งหมด" value="สถานที่ทั้งหมด"><PushpinFilled /> สถานที่ทั้งหมด</Option>
-              {data.provinces.map(province => {
-                return <Option key={province} value={province}>{province}</Option>;
+            <Select
+              defaultValue="พื้นที่ใกล้ฉัน"
+              style={{ width: "100%" }}
+              onChange={this.handleProvinceChange}
+            >
+              <Option key="พื้นที่ใกล้ฉัน" value="พื้นที่ใกล้ฉัน">
+                <PushpinFilled /> พื้นที่ใกล้ฉัน
+              </Option>
+              <Option key="สถานที่ทั้งหมด" value="สถานที่ทั้งหมด">
+                <PushpinFilled /> สถานที่ทั้งหมด
+              </Option>
+              {data.provinces.map((province) => {
+                return (
+                  <Option key={province} value={province}>
+                    {province}
+                  </Option>
+                );
               })}
             </Select>
             <b>ราคา</b>
-            <Select defaultValue="ไม่เกิน 100 บาท" style={{ width: "100%" }} onChange={this.handlePriceRangeChange}>
-              {data.priceRange.map(price => {
-                return <Option key={price} value={price}>{price}</Option>;
+            <Select
+              defaultValue="ไม่เกิน 100 บาท"
+              style={{ width: "100%" }}
+              onChange={this.handlePriceRangeChange}
+            >
+              {data.priceRange.map((price) => {
+                return (
+                  <Option key={price} value={price}>
+                    {price}
+                  </Option>
+                );
               })}
             </Select>
-            {this.state.shopType !== -1 ? <div>
-              <b>ประเภท {data.categories[this.state.shopType].name}</b>
-              <Radio.Group onChange={this.handleSubTypeChange}>
-                {data.categories[this.state.shopType].subcategories.map(subcategory => {
-                  return <Radio key={subcategory} style={radioStyle} value={subcategory}>{subcategory}</Radio>;
-                })}
-
-              </Radio.Group>
-              </div> : null}
-            
+            {this.state.shopType !== -1 ? (
+              <div>
+                <b>ประเภท {data.categories[this.state.shopType].name}</b>
+                <Radio.Group onChange={this.handleSubTypeChange}>
+                  {data.categories[this.state.shopType].subcategories.map(
+                    (subcategory) => {
+                      return (
+                        <Radio
+                          key={subcategory}
+                          style={radioStyle}
+                          value={subcategory}
+                        >
+                          {subcategory}
+                        </Radio>
+                      );
+                    }
+                  )}
+                </Radio.Group>
+              </div>
+            ) : null}
           </Sider>
-          <Content style={{marginLeft:"50px"}}>
-              {data.merchants.filter(merchant => 
-              merchant.subcategoryName === this.state.subType 
-              || this.state.shopType === -1).map(merchant => {
-                  return (<div><Row gutter={16} style={{backgroundColor:"white", border:"0.5px solid gray", padding:"10px"}}>
-                <Col lg={6}  md={9} sm={24}>
-                   <img src={merchant.coverImageId} alt="coverID" style={{objectFit:"cover", width:"100%", height:"200px"}} />
-                </Col>
-                <Col lg={18} md={15} sm={24}>
-                  <span>{merchant.shopNameTH} {merchant.isOpen === 'Y' ? <Tag color="green">เปิดอยู่</Tag> : <Tag color="gray">ปิดแล้ว</Tag>}  </span>
-                  <p>{merchant.subcategoryName} | {merchant.addressDistrictName + " " + merchant.addressProvinceName}</p>
-                  <Rate character="฿" value={merchant.priceLevel} style={{color:"gray"}} disabled/><hr/>
-                  <p>สินค้าแนะนำ : {merchant.recommendedItems.map(item => { return item + " ";})}</p>
-                </Col>
-
-            </Row><br/></div>);
+          <Content style={{ marginLeft: "50px" }}>
+            {data.merchants
+              .filter(
+                (merchant) =>
+                  merchant.subcategoryName === this.state.subType ||
+                  merchant.addressProvinceName === this.state.province
+              )
+              .map((merchant) => {
+                return (
+                  <div>
+                    <Row
+                      gutter={16}
+                      style={{
+                        backgroundColor: "white",
+                        border: "0.5px solid gray",
+                        padding: "10px",
+                      }}
+                    >
+                      <Col lg={6} md={9} sm={24}>
+                        <img
+                          src={merchant.coverImageId}
+                          alt="coverID"
+                          style={{
+                            objectFit: "cover",
+                            width: "100%",
+                            height: "200px",
+                          }}
+                        />
+                      </Col>
+                      <Col lg={18} md={15} sm={24}>
+                        <span>
+                          {merchant.shopNameTH}{" "}
+                          {merchant.isOpen === "Y" ? (
+                            <Tag color="green">เปิดอยู่</Tag>
+                          ) : (
+                            <Tag color="gray">ปิดแล้ว</Tag>
+                          )}{" "}
+                        </span>
+                        <p>
+                          {merchant.subcategoryName} |{" "}
+                          {merchant.addressDistrictName +
+                            " " +
+                            merchant.addressProvinceName}
+                        </p>
+                        <Rate
+                          character="฿"
+                          value={merchant.priceLevel}
+                          style={{ color: "gray" }}
+                          disabled
+                        />
+                        <hr />
+                        <p>
+                          สินค้าแนะนำ :{" "}
+                          {merchant.recommendedItems.map((item) => {
+                            return item + " ";
+                          })}
+                        </p>
+                      </Col>
+                    </Row>
+                    <br />
+                  </div>
+                );
               })}
-            
-          
-
           </Content>
         </Layout>
       </Layout>
